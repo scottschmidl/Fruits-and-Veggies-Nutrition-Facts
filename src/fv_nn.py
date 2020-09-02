2567eafdc291bdead59d3991a5d8ff08ee7a2a90
Scott Joseph Schmidl II <sjschmidl@gmail.com>
2020-09-01 18:05:18 -0700

    slight corrections to NB class

--- src/fv_nn.py
+++ src/fv_nn.py
@@ -61,8 +61,8 @@ if __name__ == '__main__':
     y = []
     y_enumerated = []
     all_fru_veg = os.listdir('data/fruits_vegetables')[10:41:27]
-    fru_veg = FruitsVeggiesNB
-    X, y, _ = fru_veg.get_X_y_fv(X, y, all_fru_veg)
+    fru_veg_class = FruitsVeggiesNB(X, y, all_fru_veg)
+    X, y, _ = fru_veg_class.get_X_y_fv(X, y, all_fru_veg)
     for fruit in y:
         if fruit == 'Tomato':
             y_enumerated.append(1)
