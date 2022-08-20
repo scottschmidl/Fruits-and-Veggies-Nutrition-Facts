a13893cc6183e2dd2b678f619bc4e7b4008dfe9e
scottschmidl <sjschmidl@gmail.com>
2022-08-19 23:23:56 -0500

    updated code to make more sense

--- src/cnn.py
+++ src/cnn.py
@@ -1,7 +1,7 @@
 ### TODO: once images are loaded to AWS this needs update for multiclass ###
 ### this is currently setup for binary classification for tomatoes and pears ###
 from tensorflow.keras.layers import (Dense, Dropout, Activation, Flatten,
-                                        Conv2D, MaxPooling2D)
+                                     Conv2D, MaxPooling2D)
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.callbacks import TensorBoard, EarlyStopping
 from tensorflow.keras.models import Sequential
